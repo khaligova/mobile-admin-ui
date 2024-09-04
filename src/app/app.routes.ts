a153241a0e6a9ca37c1b9cd39f8aa12931303c93
AzSoft <AzSoft@HOME-PC>
2024-09-04 18:15:44 +0400

    routing and login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,22 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginPageComponent } from './features/login/pages/login-page/login-page.component';
+import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+  {
+    path: 'auth',
+    component: EmptyLayoutComponent,
+    children: [
+      { path: '', component: LoginPageComponent },
+      { path: 'login', component: LoginPageComponent },
+    ],
+  },
 
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+    ],
+  },
 ];
