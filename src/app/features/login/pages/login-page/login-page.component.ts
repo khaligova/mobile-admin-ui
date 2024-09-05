ee6d175c3ba34bf8da965b798c5ab47126215151
User <User@DESKTOP-0QGLCAB>
2024-09-05 18:03:37 +0400

    login page desing done

--- src/app/features/login/pages/login-page/login-page.component.ts
+++ src/app/features/login/pages/login-page/login-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [InputTextModule,ButtonModule],
+  imports: [InputTextModule,ButtonModule,FloatLabelModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
