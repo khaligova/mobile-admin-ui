02c677d42ce35a0a1a2eb3ecf11a5090a5ee2ed8
AzSoft <AzSoft@HOME-PC>
2024-09-05 16:27:45 +0400

    fix

--- src/app/features/login/pages/login-page/login-page.component.ts
+++ src/app/features/login/pages/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule,ButtonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
